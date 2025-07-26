fc25aea6c71e28877a95e5f06dc894196b0d6d58
Daniele Buatti <danielebuatti@MacBook-Pro-7.local>
2025-07-27 09:31:39 +1000

    [dyad] Removed duplicate toast system - wrote 1 file(s), deleted 3 file(s)

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
@@ -21,8 +20,8 @@ const App = () => {
   return (
     <QueryClientProvider client={queryClient}>
       <TooltipProvider>
-        <Toaster />
-        <Sonner position="top-right" /> {/* Changed position to top-right */}
+        {/* Removed the redundant Toaster component */}
+        <Sonner position="top-right" />
         <AuthProvider>
           <BrowserRouter>
             <Routes>
