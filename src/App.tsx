9094c3fdbb3c8985e9c65d4019e5567c7d8b829d
Daniele Buatti <danielebuatti@MacBook-Pro-7.local>
2025-07-30 10:57:28 +1000

    [dyad] wrote 1 file(s)

--- src/App.tsx
+++ src/App.tsx
@@ -71,51 +71,49 @@ const AppContent = () => {
   }, []);
 
   return (
-    <BrowserRouter>
-      <Sidebar>
-        <div className={cn(
-          "flex-1 flex flex-col",
-          isFocusModeActive && "border-l-4 border-primary-foreground"
-        )}>
-          <Routes>
-            <Route 
-              path="/" 
-              element={
-                <Index 
-                  setIsAddTaskOpen={setIsAddTaskOpen} 
-                  currentDate={currentDate} 
-                  setCurrentDate={setCurrentDate} 
-                  manualFocusTaskId={manualFocusTaskId} // Pass down
-                  onSetAsFocusTask={onSetAsFocusTask} // Pass down
-                  onClearManualFocus={onClearManualFocus} // Pass down
-                />
-              } 
-            />
-            <Route path="/analytics" element={<Analytics currentDate={currentDate} setCurrentDate={setCurrentDate} />} />
-            <Route path="/settings" element={<Settings />} />
-            <Route path="/help" element={<Help />} />
-            <Route path="/archive" element={<Archive currentDate={currentDate} setCurrentDate={setCurrentDate} />} />
-            <Route path="/focus" element={<ProductivityTimer currentDate={currentDate} setCurrentDate={setCurrentDate} />} />
-            <Route path="/projects" element={<ProjectBalanceTracker />} />
-            <Route path="/schedule" element={<TimeBlockSchedule />} />
-            <Route path="/meditation" element={<Meditation />} />
-            <Route path="/sleep" element={<SleepTracker />} />
-            {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
-            <Route path="*" element={<NotFound />} />
-          </Routes>
-        </div>
-      </Sidebar>
-      <CommandPalette 
-        isAddTaskOpen={isAddTaskOpen} 
-        setIsAddTaskOpen={setIsAddTaskOpen} 
-        currentDate={currentDate} 
-        setCurrentDate={setCurrentDate} 
-        nextAvailableTask={nextAvailableTask}
-        manualFocusTaskId={manualFocusTaskId} // Pass down
-        onSetAsFocusTask={onSetAsFocusTask} // Pass down
-        onClearManualFocus={onClearManualFocus} // Pass down
-      />
-    </BrowserRouter>
+    <Sidebar>
+      <div className={cn(
+        "flex-1 flex flex-col",
+        isFocusModeActive && "border-l-4 border-primary-foreground"
+      )}>
+        <Routes>
+          <Route 
+            path="/" 
+            element={
+              <Index 
+                setIsAddTaskOpen={setIsAddTaskOpen} 
+                currentDate={currentDate} 
+                setCurrentDate={setCurrentDate} 
+                manualFocusTaskId={manualFocusTaskId} // Pass down
+                onSetAsFocusTask={onSetAsFocusTask} // Pass down
+                onClearManualFocus={onClearManualFocus} // Pass down
+              />
+            } 
+          />
+          <Route path="/analytics" element={<Analytics currentDate={currentDate} setCurrentDate={setCurrentDate} />} />
+          <Route path="/settings" element={<Settings />} />
+          <Route path="/help" element={<Help />} />
+          <Route path="/archive" element={<Archive currentDate={currentDate} setCurrentDate={setCurrentDate} />} />
+          <Route path="/focus" element={<ProductivityTimer currentDate={currentDate} setCurrentDate={setCurrentDate} />} />
+          <Route path="/projects" element={<ProjectBalanceTracker />} />
+          <Route path="/schedule" element={<TimeBlockSchedule />} />
+          <Route path="/meditation" element={<Meditation />} />
+          <Route path="/sleep" element={<SleepTracker />} />
+          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </div>
+    </Sidebar>
+    <CommandPalette 
+      isAddTaskOpen={isAddTaskOpen} 
+      setIsAddTaskOpen={setIsAddTaskOpen} 
+      currentDate={currentDate} 
+      setCurrentDate={setCurrentDate} 
+      nextAvailableTask={nextAvailableTask}
+      manualFocusTaskId={manualFocusTaskId} // Pass down
+      onSetAsFocusTask={onSetAsFocusTask} // Pass down
+      onClearManualFocus={onClearManualFocus} // Pass down
+    />
   );
 };
 
@@ -127,7 +125,9 @@ const App = () => {
         <AuthProvider>
           <UIProvider>
             <SoundProvider>
-              <AppContent />
+              <BrowserRouter> {/* BrowserRouter now wraps AppContent */}
+                <AppContent />
+              </BrowserRouter>
             </SoundProvider>
           </UIProvider>
         </AuthProvider>
