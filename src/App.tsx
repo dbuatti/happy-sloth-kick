0536ee4f0843377d05b183b65338e061e3268110
Daniele Buatti <danielebuatti@MacBook-Pro-7.local>
2025-08-26 16:09:27 +1000

    [dyad] Added QueryClientProvider to App.tsx - wrote 1 file(s)

--- src/App.tsx
+++ src/App.tsx
@@ -4,19 +4,24 @@ import NotFound from "./pages/NotFound.tsx";
 import { Toaster } from "@/components/ui/toaster.tsx";
 import { SessionProvider } from "./integrations/supabase/session-context.tsx";
 import Schedule from "./pages/Schedule.tsx";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 function App() {
   return (
-    <Router>
-      <SessionProvider>
-        <Routes>
-          <Route path="/" element={<Index />} />
-          <Route path="/schedule" element={<Schedule />} />
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-        <Toaster />
-      </SessionProvider>
-    </Router>
+    <QueryClientProvider client={queryClient}>
+      <Router>
+        <SessionProvider>
+          <Routes>
+            <Route path="/" element={<Index />} />
+            <Route path="/schedule" element={<Schedule />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+          <Toaster />
+        </SessionProvider>
+      </Router>
+    </QueryClientProvider>
   );
 }
 
