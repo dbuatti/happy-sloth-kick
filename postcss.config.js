2b6df07e912f967b1312533a8c3104dcedc0e255
Daniele Buatti <danielebuatti@MacBook-Pro-7.local>
2025-08-29 09:14:59 +1000

    [dyad] Updated PostCSS configuration to ensure custom theme CSS rules are not purged during the build process. - wrote 2 file(s)

--- postcss.config.js
+++ postcss.config.js
@@ -1,5 +1,12 @@
+import tailwindcss from 'tailwindcss';
+import autoprefixer from 'autoprefixer';
+import postcssImport from 'postcss-import';
+import tailwindcssNesting from 'tailwindcss/nesting';
+
 export default {
   plugins: {
+    'postcss-import': {},
+    'tailwindcss/nesting': {},
     tailwindcss: {},
     autoprefixer: {},
   },
