a8fa48cdd1a11f19b76c2195e73a532d42d6479d
Daniele Buatti <danielebuatti@MacBook-Pro-7.local>
2025-08-28 18:29:23 +1000

    [dyad] Wrapped app with SessionContextProvider in App.tsx - wrote 1 file(s)

--- src/App.tsx
+++ src/App.tsx
@@ -5,18 +5,21 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { Index } from "./pages/Index";
 import MainLayout from "./components/MainLayout";
 import NotFound from "./pages/NotFound";
+import { SessionContextProvider } from "./integrations/supabase/auth"; // Import SessionContextProvider
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<MainLayout />}>
-          <Route index element={<Index />} />
-          {/* Add other routes here as you create new pages */}
-          <Route path="*" element={<NotFound />} />
-        </Route>
-      </Routes>
-    </Router>
+    <SessionContextProvider> {/* Wrap the entire router with the context provider */}
+      <Router>
+        <Routes>
+          <Route path="/" element={<MainLayout />}>
+            <Route index element={<Index />} />
+            {/* Add other routes here as you create new pages */}
+            <Route path="*" element={<NotFound />} />
+          </Route>
+        </Routes>
+      </Router>
+    </SessionContextProvider>
   );
 }
 
